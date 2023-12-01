7f0a6c79367ad176618d944d33d0e27c45044022
Christopher Greene <chigreenwork@gmail.com>
2023-11-30 19:13:12 -0500

    corrects thoughts not being seeded in users when seed is run

--- utils/seed.js
+++ utils/seed.js
@@ -5,32 +5,28 @@ const { userData, thoughtData } = require("./data");
 async function seedDatabase() {
   await connection.once("open", async () => {
     try {
+      // Clear existing data
       await User.deleteMany({});
       await Thought.deleteMany({});
 
-      // Seed users
-      const users = await User.insertMany(userData);
+      // Seed Users
+      const seededUsers = await User.insertMany(userData);
+      console.log("Users seeded");
 
-      // Map usernames to user IDs for quick lookup
-      const userMap = users.reduce((map, user) => {
-        map[user.username] = user._id;
-        return map;
-      }, {});
-
-      // Associate each thought with the userID and save thoughts
-      const thoughtsWithUsers = thoughtData.map((thought) => {
-        return { ...thought, userId: userMap[thought.username] };
-      });
-      const savedThoughts = await Thought.insertMany(thoughtsWithUsers);
+      // Seed Thoughts
+      const seededThoughts = await Thought.insertMany(thoughtData);
+      console.log("Thoughts seeded");
 
       // Update each user's thoughts array
-      for (const thought of savedThoughts) {
-        await User.findByIdAndUpdate(
-          thought.userId,
-          { $push: { thoughts: thought._id } },
-          { new: true }
-        );
+      for (const thought of seededThoughts) {
+        const user = seededUsers.find((u) => u.username === thought.username);
+        if (user) {
+          await User.findByIdAndUpdate(user._id, {
+            $push: { thoughts: thought._id },
+          });
+        }
       }
+      console.log("Users updated with thoughts");
 
       console.log("Database seeded successfully!");
     } catch (err) {
